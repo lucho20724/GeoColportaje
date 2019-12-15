48747a5bd6d190191e3841d82f22d1d5310ce301
Luciano Hinojosa <luciano.94.33@gmail.com>
2019-12-15 19:16:16 -0300

    Changes until 15-12

---
+++ app/src/main/java/uap/geocolportaje/geocoportaje/Informes/informeventaActivity.java
@@ -0,0 +1,15 @@
+package uap.geocolportaje.geocoportaje.Informes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uap.geocolportaje.geocoportaje.R;
+
+public class informeventaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informeventa);
+    }
+}
