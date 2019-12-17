3da172abe2c21a99a4b5a6d8c1b66abc432dcbe2
Luciano Hinojosa <luciano.94.33@gmail.com>
2019-12-17 03:41:17 -0300

    Changes until 17-12

---
+++ app/src/main/java/uap/geocolportaje/geocoportaje/Informes/informepuntosActivity.java
@@ -0,0 +1,15 @@
+package uap.geocolportaje.geocoportaje.Informes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uap.geocolportaje.geocoportaje.R;
+
+public class informepuntosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informepuntos);
+    }
+}
